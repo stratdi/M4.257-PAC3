6a21c1cefa17daf216c60c2a25f8196e0444a1cd
Jordi Cruz Lladó <jcl336@uoc.edu>
2024-01-09 11:31:45 +0100

    Fix JS

--- src/js/theme.js
+++ src/js/theme.js
@@ -4,43 +4,47 @@
  * @param {*} save 
  */
 function setTheme(theme, save) {
-    const html = document.documentElement;
-    const selectedTheme = theme === "osdefault"
-        ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
-        : theme;
+  const html = document.documentElement;
+  const selectedTheme = theme === "osdefault"
+    ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
+    : theme;
 
-    html.dataset.theme = selectedTheme;
+  html.dataset.theme = selectedTheme;
 
-    if (save) {
-        localStorage.setItem('ring-theme', selectedTheme);
-    }
+  if (save) {
+    localStorage.setItem('ring-theme', selectedTheme);
+  }
 }
 
 /**
  * Inicialitza el tema de la pàgina. Mira si l'usuari ha seleccionat cap opció prèviament del localStorage. Si no, per defecte mira la config del SO.
  */
 function setThemeInitial() {
-    const theme = localStorage.getItem('ring-theme');
+  const theme = localStorage.getItem('ring-theme');
 
-    if (theme) {
-        setTheme(theme, false);
-        document.querySelector(`#theme-${theme}-radio`).checked = true;
-    } else {
-        setTheme('osdefault', false);
-    }
+  if (theme) {
+    setTheme(theme, false);
+    document.querySelector(`#theme-${theme}-radio`).checked = true;
+  } else {
+    setTheme('osdefault', false);
+  }
 }
 
 /**
  * Inicialitza la funcionalitat dels botons del tema.
  */
 function initTheme() {
-    const lightButton = document.querySelector("#theme-light");
-    const darkButton = document.querySelector("#theme-dark");
-    const osButton = document.querySelector("#theme-osdefault");
+  const lightButton = document.querySelector("#theme-light");
+  const darkButton = document.querySelector("#theme-dark");
+  const osButton = document.querySelector("#theme-osdefault");
 
-    lightButton.addEventListener("click", () => setTheme('light', true));
-    darkButton.addEventListener("click", () => setTheme('dark', true));
-    osButton.addEventListener("click", () => setTheme('osdefault', true));
+  lightButton.addEventListener("click", () => setTheme('light', true));
+  darkButton.addEventListener("click", () => setTheme('dark', true));
+  osButton.addEventListener("click", () => setTheme('osdefault', true));
 
-    setThemeInitial();
+  setThemeInitial();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  initTheme();
+});
